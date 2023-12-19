e05bfd8138bd69647f50bdeb88446451438ed3a0
github automation <github-automation@budai.cz>
2023-12-19 00:54:22 +0000

    Update to 9b53d3e6a66ae8f2dd9845a160475a20f1617161

--- cmd/osbuild-composer/main.go
+++ cmd/osbuild-composer/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/coreos/go-systemd/activation"
 	slogger "github.com/ondrejbudai/osbuild-composer-public/pkg/splunk_logger"
 	"github.com/sirupsen/logrus"
-	"github.com/sirupsen/logrus/hooks/syslog"
 )
 
 const (
@@ -63,7 +62,7 @@ func main() {
 	}
 
 	if config.SplunkHost != "" {
-		hook, err := slogger.NewSplunkHook(context.Background(), config.SplunkHost, config.SplunkPort, config.SplunkToken, "composer")
+		hook, err := slogger.NewSplunkHook(context.Background(), config.SplunkHost, config.SplunkPort, config.SplunkToken, "osbuild-composer")
 
 		if err != nil {
 			panic(err)
@@ -79,15 +78,6 @@ func main() {
 	cacheDir, ok := os.LookupEnv("CACHE_DIRECTORY")
 	if !ok {
 		logrus.Fatal("CACHE_DIRECTORY is not set. Is the service file missing CacheDirectory=?")
-	}
-
-	if len(config.SyslogServer) > 0 {
-		hook, err := syslog.NewSyslogHook("tcp", config.SyslogServer, 0, "osbuild-composer")
-		if err != nil {
-			logrus.Fatal("Error connecting to syslog: " + err.Error())
-		}
-
-		logrus.AddHook(hook)
 	}
 
 	composer, err := NewComposer(config, stateDir, cacheDir)
